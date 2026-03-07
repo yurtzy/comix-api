bc3dc6c058c2cea9ec3179aa6621a74214331790
yurtzy <lionelgaming256@gmail.com>
2026-03-07 08:12:10 +0800

    fix(api): setup CORS configuration in nextjs to allow client-side searching from comix-ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // Setup CORS for API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // or specify your 'https://comix-ui.vercel.app'
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
